03c22feb27f54dd04cee38f80c6da61a14593a40
Wayne <xf.wong@qq.com>
2018-06-05 20:07:34 +0800

    第一次上传helloworld.go文件

---
+++ goos.go
@@ -0,0 +1,16 @@
+//go run
+package main
+
+import (
+	"fmt"
+	// "os"
+	// "runtime"
+)
+
+func main() {
+	// var goos string = runtime.GOOS
+	// fmt.Printf("The operating system is: %s\n", goos)
+	// path := os.Getenv("PATH")
+	// fmt.Printf("Path is %s\n", path)
+	fmt.Println("hello world")
+}
