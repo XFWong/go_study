b19ac8b361ac0d8f289d6c6677f9b46d221780da
Wayne <xf.wong@qq.com>
2018-06-13 20:29:02 +0800

    添加结构体

---
+++ 20180613/treesort_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"math/rand"
+	"sort"
+	"testing"
+
+	"20180613/treesort"
+)
+
+func TestSort(t *testing.T) {
+	data := make([]int, 50)
+	for i := range data {
+		data[i] = rand.Int() % 50
+	}
+	treesort.Sort(data)
+	if !sort.IntsAreSorted(data) {
+		t.Errorf("not sorted: %v", data)
+	}
+}
+
+func main() {
+
+}
