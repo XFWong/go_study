5c37cf7bfd583597ad86b3ba814da474e65b8c62
Wayne <xf.wong@qq.com>
2018-06-14 16:43:53 +0800

    匿名函数调用

---
+++ 20180614/squares.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func squares() func() int {
+	var x int
+	fmt.Printf("line:9 x:%d\n", x)
+	return func() int {
+		fmt.Printf("line:11 x:%d\n", x)
+		x++
+		return x * x
+	}
+}
+
+func main() {
+	fmt.Println("******19******")
+	f := squares()
+	fmt.Println("******20******")
+	fmt.Println(f()) // "1"
+	fmt.Println("******22******")
+	fmt.Println(f()) // "4"
+	fmt.Println("******24******")
+	fmt.Println(f()) // "9"
+	fmt.Println("******26******")
+	fmt.Println(f()) // "16"
+	fmt.Println("******28******")
+}
