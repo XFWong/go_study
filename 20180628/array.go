12bbf9cb86b8b8441cd23124a0e392a17235c7f2
Wayne <xf.wong@qq.com>
2018-06-28 15:33:31 +0800

    slice 学习

--- 20180628/array.go
+++ 20180628/array.go
@@ -4,12 +4,40 @@ import (
 	"fmt"
 )
 
+func nonempty(strings []string) []string {
+	i := 0
+	for d, s := range strings {
+		fmt.Println(d, i)
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	fmt.Printf("%d\n", i)
+	return strings[:i]
+}
+
+// func nonempty2(strings []string) []string {
+// 	out := strings[:0]
+// 	for d, s := range strings {
+// 		fmt.Println(d, i)
+// 		if s != "" {
+// 			strings[i] = s
+// 			i++
+// 		}
+// 	}
+// 	fmt.Printf("%d\n", i)
+// 	return strings[:i]
+// }
+
 func main() {
 	// 1.测试数组长度
-	var (
-		a [30]int = [30]int{1, 2, 3}
-	)
-	fmt.Println(a)
-	fmt.Println(len(a))
-
+	// var (
+	// 	a [30]int = [30]int{1, 2, 3}
+	// )
+	// fmt.Println(a)
+	// fmt.Println(len(a))
+	date := []string{"one", "", "three"}
+	fmt.Printf("%q\n", nonempty(date))
+	fmt.Printf("%q\n", date)
 }
