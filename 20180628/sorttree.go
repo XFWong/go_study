977451e7e8711059e99edd3f6d3c41d612616a16
Wayne <xf.wong@qq.com>
2018-06-28 20:30:01 +0800

    测试二叉树排序

--- 20180628/sorttree.go
+++ 20180628/sorttree.go
@@ -14,17 +14,29 @@ func sort(values []int) {
 	for _, v := range values {
 		root = add(root, v)
 	}
+	// for _, data := range root {
+	// 	fmt.Println(data.value)
+	// }
 	appendValues(values[:0], root)
 }
+
 func appendValues(values []int, t *tree) []int {
+	fmt.Println(values, t)
 	if t != nil {
 		values = appendValues(values, t.left)
+		fmt.Printf("line:27,t.value:%d,vlues:%v\n", t.value, values)
 		values = append(values, t.value)
+		fmt.Printf("line:29,t.value:%d,vlues:%v\n", t.value, values)
 		values = appendValues(values, t.right)
 	}
+	fmt.Printf("line:32,vlues:%v\n", values)
 	return values
 }
+
 func add(t *tree, value int) *tree {
+	fmt.Printf("value:%d\n", value)
+	fmt.Printf("t:%v,%T\n", t, t)
+
 	if t == nil {
 		t = new(tree)
 		t.value = value
@@ -38,7 +50,7 @@ func add(t *tree, value int) *tree {
 	return t
 }
 func main() {
-	values := []int{28, 3, 4, 5, 6}
+	values := []int{5, 4, 4, 3, 2, 1}
 	sort(values)
 	fmt.Println(values)
 }
